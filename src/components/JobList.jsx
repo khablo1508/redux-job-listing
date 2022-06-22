6bc4a0077f8580debec73f538a0a050a15dd1405
khablo1508 <khablo.anna@gmail.com>
2022-06-22 13:02:36 +0200

    initial commit

---
+++ src/components/JobList.jsx
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { JobPosition } from './JobPosition';
+import {
+  selectAllPositions,
+  selectVisiblePositions,
+} from 'store/positions/position-selectors';
+import { selectFilters } from 'store/filters/filters-selectors';
+
+import { addFilter } from 'store/filters/filters-actions';
+
+const JobList = () => {
+  const dispatch = useDispatch();
+  const currentFilters = useSelector(selectFilters);
+  const positions = useSelector((state) =>
+    selectVisiblePositions(state, currentFilters)
+  );
+
+  const handleAddFilter = (filter) => {
+    dispatch(addFilter(filter));
+  };
+
+  return (
+    <div className='job-list'>
+      {positions.map((item) => (
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
+      ))}
+    </div>
+  );
+};
+
+export { JobList };
